4e23e3e2328f0e2511221311ee406e88313c15a1
ii-cookie <isaacnleong@gmail.com>
2025-06-19 11:01:26 +0800

    changed tilt start pos and cleaned up codestat a bit

--- codestats.js
+++ codestats.js
@@ -1,25 +1,11 @@
-console.log('hi');
-// var all_data;
-// var lang;
-
-// fetchdata();
-
-// async function fetchdata(){
-//     const response = await fetch("https://codestats.net/api/users/iicookie")
-//         .then(res => res.json())
-//         .then(data => {
-//             all_data = data;
-//             console.log(data.languages)})
-//         .catch(error => console.error(error));
-//     console.log(all_data);
-// }
 fetchData();
 
 async function fetchData(){
-
     try{
         let languages = [];
         let language = [];
+        let language_list = [];
+        let xp_list = [];
         
         await fetch('https://codestats.net/api/users/iicookie')
         .then(res=>{
@@ -41,38 +27,23 @@ async function fetchData(){
 
         // imgElement.src = pokemonSprite;
         // imgElement.style.display = "block";
+        console.log(languages)
         c=0;
         for (language in languages){
             ++c
             console.log(language)   //name of each language here
+            language_list.push(language)
             xp = languages[language].xps
             console.log(xp)         //xp in this language here
+            xp_list.push(xp);
         }
         console.log(c)  //total number of languages here
-        
+        console.log(language_list)  //list of languages here
+        console.log(xp_list)        //list of xp here
+
+        return language_list, xp_list;
     }
     catch(error){
         console.error(error);
     }
 }
-
-
-
-// await fetch('https://codestats.net/api/users/iicookie')
-//     .then(res=>{
-//         if(!res.ok){
-//             console.log('Problem');
-//             return;
-//         }
-//         return res.json();
-//     })
-//     .then(data=>{
-//         languages=data.languages;
-//     })
-//     .catch(error=>{
-//         console.log('ERROR')
-//     });
-
-
-// console.log(languages);
-// console.log(languages.CSS);
