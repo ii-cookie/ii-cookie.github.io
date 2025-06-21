93e5ccd842f523c29f48abf7487ea6d970791da7
ii-cookie <isaacnleong@gmail.com>
2025-06-21 15:06:06 +0800

    unused particle effect

---
+++ particle_effect.js
@@ -0,0 +1,6 @@
+//reference to https://www.youtube.com/watch?v=W78MY7_q_6A
+//lowkey kinda given up, may implement later
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const img = new Image();
